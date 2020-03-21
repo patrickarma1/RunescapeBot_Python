870bb17b96b377f781ca5ad8a25ba8545400e774
patrickarma1 <36601548+patrickarma1@users.noreply.github.com>
2020-03-21 05:47:51 -0700

    Create mouse_pos.py

---
+++ mouse_pos.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+# displays x and y coordinates of mouse updating in console without multiple lines
+
+def get_pos():
+    pos = pyautogui.position()
+    print("\r", pos, end="")
+while True:
+    get_pos()
