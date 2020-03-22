bb363f9295f308311a3fa41184e3db44b1ad28be
patrickarma1 <36601548+patrickarma1@users.noreply.github.com>
2020-03-21 20:55:02 -0700

    mouse.py with exception handling on imports
    added exception handling

--- mouse_pos.py
+++ mouse_pos.py
@@ -2,7 +2,8 @@ import subprocess
 try:
 import pyautogui
 except ImportError:
-    
+    subprocess.Popen(['pip install pyautogui']stdout=subprocess.PIPE,universal_newlines=True)
+   print("\r""installing pyautogui",end='')
 # displays x and y coordinates of mouse updating in console without multiple lines
 
 def get_pos():
