7364c59ea6a10a65c95f7be566814fef34d5a32a
patrickarma1 <36601548+patrickarma1@users.noreply.github.com>
2020-03-21 21:10:32 -0700

    Update mouse_pos.py
    added breakpoint

--- mouse_pos.py
+++ mouse_pos.py
@@ -6,10 +6,21 @@ try:
 except ImportError:
     subprocess.call('pip install pyautogui')
     print("\r""installing pyautogui",end='')
+try:
+    from keyboard import is_pressed
+    print("keyboard imported")
+except ImportError:
+    subprocess.call('pip install keyboard')
+    print("\r""installing keyboard",end='')
 # displays x and y coordinates of mouse updating in console without multiple lines
 
 def get_pos():
     pos = pyautogui.position()
     print("\r", pos, end="")
-while True:
-    get_pos()
+
+q = (keyboard.is_pressed("q"))
+While q is False:
+     print("[+] Detected breakpoint ..... Exiting.")
+      get_pos()
+      if q is True:
+        break
